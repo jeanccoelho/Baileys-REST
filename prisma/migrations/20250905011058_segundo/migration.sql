9a377c1eca7e1ab61a3ce3101bd7c9d67f327ba3
Jean Carlos Coelho <jeancoelho@live.com>
2025-09-04 22:12:22 -0300

    ...

---
+++ prisma/migrations/20250905011058_segundo/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "public"."contacts" ADD COLUMN     "whatsapp_address" TEXT,
+ADD COLUMN     "whatsapp_business_hours" TEXT,
+ADD COLUMN     "whatsapp_category" TEXT,
+ADD COLUMN     "whatsapp_email" TEXT,
+ADD COLUMN     "whatsapp_website" TEXT;
